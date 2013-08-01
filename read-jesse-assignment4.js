d2133d0200da1568656d07c371edefcaf371448c
Jesse Read <darthnerdus@me.com>
2013-08-01 16:17:44 -0400

    Added isURL function and tests.
    Adjusted whitespacing.

--- read-jesse-assignment4.js
+++ read-jesse-assignment4.js
@@ -26,13 +26,34 @@ var stringLibrary = function () {
 			return false;
 		}
 	}
+	
+	var isURL = function (val) {
+		if (val.substr(0, 4) === "http") { 
+			if (val.substr(4, 3) === "://" || val.substr(4, 4) === "s://") {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
 	return {
 		"isPhoneNumber": isPhoneNumber,
+		"isURL": isURL
 	};
 }
+
 var myLibrary = new stringLibrary();
 console.log("PHONE NUMBER TESTS");
 console.log("661-665-5585: " + myLibrary.isPhoneNumber("661-665-5585"));
 console.log("661-665-a585: " + myLibrary.isPhoneNumber("661-665-a585"));
 console.log("222: " + myLibrary.isPhoneNumber("222"));
 console.log("8881115555: " + myLibrary.isPhoneNumber("8881115555"));
+
+console.log("HTTP(S) URL TESTS");
+console.log("http://google.com/: " + myLibrary.isURL("http://google.com/"));
+console.log("https://fullsail.edu/: " + myLibrary.isURL("https://fullsail.edu/"));
+console.log("httpgoogle.com: " + myLibrary.isURL("httpgoogle.com"));
+console.log("ftp://fileserver.local: " + myLibrary.isURL("ftp://fileserver.local"));
