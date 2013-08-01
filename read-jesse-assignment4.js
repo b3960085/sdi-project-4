31aa4f4ad136cbdb1cf93904d057ad56dfebd8dd
Jesse Read <darthnerdus@me.com>
2013-08-01 16:01:34 -0400

    Setup library with isPhoneNumber function.

--- read-jesse-assignment4.js
+++ read-jesse-assignment4.js
@@ -1,1 +1,32 @@
-alert("JavaScript works!");
+/*  Jesse Read
+    Project 4
+    08/01/13 */
+   
+var stringLibrary = function () {
+	var isPhoneNumber = function (val) {
+		var inputLength = val.length;
+		var dashPosition = [];
+		if (inputLength === 12) {
+			for (i = 0; i < inputLength; i++) {
+				if(isNaN(val[i]) && val[i] != "-") {
+					return false;
+				} else {
+					if(val[i] === "-") {
+						dashPosition.push(i);
+					}
+				}
+			}
+			console.log();
+			if (JSON.stringify(dashPosition) === JSON.stringify([3,7])) {
+				return true;
+			} else {
+				return false;
+			}
+		} else { 
+			return false;
+		}
+	}
+	return {
+		"isPhoneNumber": isPhoneNumber,
+	};
+}
